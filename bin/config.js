c22286b12b8214b618d66f53f32a73669e71a796
zjhiphop <zjhiphop@gmail.com>
2011-12-22 15:13:49 +0800

    add v0.1.0

---
+++ bin/config.js
@@ -0,0 +1,24 @@
+module.exports ={
+  http:{
+      host : 'cns-812',
+      port : 8111,
+      method : 'GET'
+  },
+  lib:{
+    "backbone":'/et_refac/Client/script/lib/backbone.js',
+    "mustache":'/et_refac/Client/script/lib/mustache.js',
+    "r":'/et_refac/Client/script/lib/r.js',
+    "jtemplate":'/et_refac/Client/script/lib/jtemplate.js',
+    "underscore":'/et_refac/Client/script/lib/underscore.js',
+    "require":'/et_refac/Client/script/lib/require.js',
+    "jquery-ui":'/et_refac/Client/script/lib/require.js'
+  },
+  helper:{
+    "require-jquery":'/et_refac/Client/script/helper/require-jquery.js',
+    "order":'/et_refac/Client/script/helper/order.js',
+    "text":'/et_refac/Client/script/helper/text.js'
+  },
+  css:{
+    "reset":'/et_refac/Client/css/reset-min.css'
+  }
+}
